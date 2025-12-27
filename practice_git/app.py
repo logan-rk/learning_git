b23c0b599c4dbd17b7689184ec87ae232e5190e8
Raj Thakur <raaz927logan@gmail.com>
2025-12-27 07:30:09 +0530

    Clean up comments and resolve merge conflict

--- practice_git/app.py
+++ practice_git/app.py
@@ -7,4 +7,4 @@ def division(a,b):
     return a/b
     ''' this is the conflict comment need to be fix '''
 
-'''developer a reporting --- nothing here need to change'''
+'''this is trash remove this ....developer a reporting --- nothing here need to change''' 
